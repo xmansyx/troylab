09cabdc95f8d134b8e3e099c24d3c9ee90a5b769
Mansy <xmansyx@gmail.com>
2022-07-25 15:15:27 +0200

    creating migration files for schools and students

--- database/migrations/2022_07_25_125754_create_students_table.php
+++ database/migrations/2022_07_25_125754_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->unsignedBigInteger('schoole_id');
+            $table->unsignedInteger('order');
             $table->timestamps();
+
+            $table->foreign('schoole_id')->references('id')->on('schools');
         });
     }
 
@@ -31,6 +30,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('students');
     }
 }
